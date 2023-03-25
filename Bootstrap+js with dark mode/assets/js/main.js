82631bcad65e6d521b63a6055b07f6c6ff7c9bf9
Kamal-Alloush <kamal132k@gmail.com>
2023-03-25 17:16:00 +0200

    do some editing

--- Bootstrap+js with dark mode/assets/js/main.js
+++ Bootstrap+js with dark mode/assets/js/main.js
@@ -66,30 +66,7 @@ const tabs =document.querySelectorAll ('[data-target]'),
             })
         })
 
-/*==================== SERVICES MODAL ====================*/
 
-const modalViews = document.querySelectorAll('.services__modal'),
-        modalBtns = document.querySelectorAll('.services__button'),
-        modalCloses = document.querySelectorAll('.services__modal-close')
-
-let modal = function(modalClick){
-    modalViews[modalClick].classList.add('active-modal')
-}        
-
-modalBtns.forEach((modalBtn, i) =>{
-    modalBtn.addEventListener('click', () =>{
-        modal(i)
-    })
-
-
-modalCloses.forEach((modalCloses)=>{
-modalCloses.addEventListener('click',() =>{
-    modalViews.forEach((modalView) =>{
-        modalView.classList.remove('active-modal')
-    })
-})
-})    
-})
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 let swiperPortfolio = new Swiper('.portfolio__container', {
@@ -105,23 +82,6 @@ let swiperPortfolio = new Swiper('.portfolio__container', {
     },
   });
 
-/*==================== TESTIMONIAL ====================*/
-let swiperTestimonial = new Swiper('.testimonial__container', {
-    loop:true,
-    grabCursor:true,
-
- 
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-      dynamicBullets: true,
-    },
-    breakpoints:{
-        568:{
-            slidesPerView: 2,
-        }
-    }
-  });
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
 const sections = document.querySelectorAll('section[id]')
